328a532f44115b1733dc8a404a69782d058c76fe
yang <yang282850228@gmail.com>
2025-09-16 12:07:42 -0400

    Completed TODO #1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,4 +1,5 @@
 public class DataTypes {
+    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(int[] numbers) {
         long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
